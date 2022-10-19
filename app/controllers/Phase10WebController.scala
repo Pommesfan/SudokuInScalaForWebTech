75aa00eda91049dd6e8eaa7acf5435bf98ca9242
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-10-19 13:36:19 +0200

    removed sudoku

--- app/controllers/Phase10WebController.scala
+++ app/controllers/Phase10WebController.scala
@@ -2,21 +2,17 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-import de.htwg.se.sudoku.Sudoku
-import de.htwg.se.sudoku.controller.controllerComponent.GameStatus
-import controllers.Controller
 import utils.ProgramStartedEvent
 import views.TUI
 
 @Singleton
-class SudokuController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class Phase10WebController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val c = new Controller
   val tui = new TUI(c)
   c.add(tui)
   c.notifyObservers(new ProgramStartedEvent) //set correct state in TUI
   tui.handle_input("PlayerA PlayerB")
-  val gameController = Sudoku.controller
-  def sudokuAsText =  gameController.gridToString + GameStatus.message(gameController.gameStatus)
+  def phase10AsText =  tui.get_last_output
 
   def about= Action {
     Ok(views.html.index())
