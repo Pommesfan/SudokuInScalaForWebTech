05dac7f5e1c1733ec86a5648b8af4722dd1c85ab
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-10-25 18:18:12 +0200

    implemented home-page for setting up players

--- app/controllers/Phase10WebController.scala
+++ app/controllers/Phase10WebController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-import utils.ProgramStartedEvent
+import utils.{DoCreatePlayerEvent, ProgramStartedEvent}
 import views.TUI
 import play.twirl.api.Html
 
@@ -12,7 +12,7 @@ class Phase10WebController @Inject()(cc: ControllerComponents) extends AbstractC
   val tui = new TUI(c)
   c.add(tui)
   c.notifyObservers(new ProgramStartedEvent) //set correct state in TUI
-  tui.handle_input("PlayerA PlayerB")
+  //tui.handle_input("PlayerA PlayerB")
 
   def about = Action {
     Ok(views.html.index())
@@ -22,9 +22,36 @@ class Phase10WebController @Inject()(cc: ControllerComponents) extends AbstractC
     Ok(views.html.help())
   }
 
+  def set_players(p1:String, p2:String, p3:String, p4:String) = {
+    def make_list(): List[String] = {
+      var l = List[String]()
+      if (!(p1.isEmpty || p2.isEmpty)) {
+        l = p1 :: l
+        l = p2 :: l
+        if (p3.isEmpty) {
+          return l
+        }
+        l = p3 :: l
+        if (p4.isEmpty) {
+          return l
+        }
+        l = p4 :: l
+      }
+      l
+    }
+
+    c.solve(new DoCreatePlayerEvent(make_list()))
+    phase10
+  }
+
   def phase10 = Action {
-    val html = Html.apply(tui.get_last_output.replace("\n", "<br>"))
-    Ok(views.html.game(html))
+    def state = c.getState
+    if (state.isInstanceOf[InitialState]) {
+      Ok(views.html.home())
+    } else {
+      val html = Html.apply(tui.get_last_output.replace("\n", "<br>"))
+      Ok(views.html.game(html))
+    }
   }
 
   def submitInput(x: String): Action[AnyContent] = {
