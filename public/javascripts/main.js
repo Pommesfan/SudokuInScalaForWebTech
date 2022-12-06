f1089b65535c96f1efe95f4c08a63dbf6d4243ae
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-12-06 18:33:25 +0100

    refresh page with da usind javascript instead of reload from server (only switch card yet)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,3 +2,51 @@ let alert_text = document.getElementById('hidden_text_alert')
 if (alert_text != null) {
     alert(alert_text.innerText)
 }
+
+function show_player_cards(cards, show_checkboxes) {
+    let html = ""
+    for (let i = 0; i < cards.length; i++) {
+        html += `
+            <div className="row">
+                <div className="col-7">
+                    ${i}: ${cardToString(cards[i])}
+                </div>
+                ${checkboxes(i, 2, show_checkboxes)}
+            </div>`
+    }
+    return html
+}
+
+function checkboxes(i, cardGroupSize, show_checkboxes) {
+    let html = ""
+    if (show_checkboxes) {
+        for (let j = 0; j < cardGroupSize; j++) {
+            html += `
+            <div class="col-1">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id=${"inlineCheckbox" + j + "_" + i}>
+                </div>
+            </div>`
+            html += "\n"
+        }
+    }
+    return html
+}
+
+function getFile(name) {
+    let data = "";
+    jQuery.get("/assets/" + name).then(response => {
+        data = response
+    })
+    return data
+}
+
+const switchCardForm = getFile("javascripts/utils.js")
+
+function update(data) {
+    if (data['event'] == "GoToDiscardEvent") {
+        document.getElementById("playerCards").innerHTML = show_player_cards(data['cardStash'], true)
+    } else {
+        document.location.reload()
+    }
+}
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -6,5 +6,21 @@ function post_data(route, json) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(json)
-    }).then(response => document.location.reload())
+    }).then(response => response.json().then(data => update(data)))
+}
+
+function cardToString(c) {
+    function getColor() {
+        switch (c['color']) {
+            case 1:
+                return "Rot"
+            case 2:
+                return "Gelb"
+            case 3:
+                return "Blau"
+            case 4:
+                return "Grün"
+        }
+    }
+    return "Farbe: " + getColor() + "; Wert = " + c['value']
 }
