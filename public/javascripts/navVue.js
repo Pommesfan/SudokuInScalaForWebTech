d701501a9724df93bab5379ab46151a8f09941b0
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-12-19 19:08:02 +0100

    Redirect from navbar

--- public/javascripts/navVue.js
+++ public/javascripts/navVue.js
@@ -17,9 +17,9 @@ appNav.component('phase10nav', {
                     <a v-if="selected === 'home'" class="nav-link active hoverable text-center" aria-current="page" id="navGame">Game</a>
                     <a v-else class="nav-link hoverable text-center" id="navGame">Game</a>
                     <a v-if="selected === 'about'" class="nav-link active hoverable text-center" aria-current="page" id="navAbout">About</a>
-                    <a v-else class="nav-link hoverable text-center" id="navAbout">About</a>
+                    <a v-else class="nav-link hoverable text-center" id="navAbout" href="/about">About</a>
                     <a v-if="selected === 'about'" class="nav-link active hoverable text-center" aria-current="page" id="navHelp">Help</a>
-                    <a v-else class="nav-link hoverable text-center" id="navHelp">Help</a>
+                    <a v-else class="nav-link hoverable text-center" id="navHelp" href="/help">Help</a>
                 </div>
             </div>             
         </nav>
