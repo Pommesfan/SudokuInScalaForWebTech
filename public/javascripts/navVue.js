ce7f933383946ea1c1622465350e928990c63c42
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-12-19 19:04:42 +0100

    navbar with vue works(problem with pictures -> removed, can be re-appended later)

--- public/javascripts/navVue.js
+++ public/javascripts/navVue.js
@@ -8,15 +8,10 @@ appNav.component('phase10nav', {
     template: `
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand " href="#"></a>
-            <picture>
-               <source media="(min-width:400px)" srcset="@routes.Assets.versioned("images/Phase10-Logo.png")">
-               <img class="picture" srcset="@routes.Assets.versioned("images/Phase10-Logo.png")" width="30" height="30" class="d-inline-block align-top" alt="">
-            </picture>
             Phase10
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a v-if="selected === 'home'" class="nav-link active hoverable text-center" aria-current="page" id="navGame">Game</a>
