06435f9833728307ab87c36372432471fae8c778
Johannes Wirbser <johannes.wirbser@gmx.de>
2023-01-09 09:56:38 +0100

    removed cardToString()

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -9,22 +9,6 @@ function post_data(route, json) {
     }).then(response => response.json().then(data => update(data)))
 }
 
-function cardToString(c) {
-    function getColor() {
-        switch (c['color']) {
-            case 1:
-                return "Rot"
-            case 2:
-                return "Gelb"
-            case 3:
-                return "Blau"
-            case 4:
-                return "Grün"
-        }
-    }
-    return "Farbe: " + getColor() + "; Wert = " + c['value']
-}
-
 function drawCard(number, color) {
     function getColor() {
         switch (color) {
