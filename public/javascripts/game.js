3078018955d36f2e6e11eb6199aa5ccdb5280341
Johannes Wirbser <johannes.wirbser@gmx.de>
2023-12-03 22:26:14 +0100

    connectWebSocket() excluded

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -341,42 +341,6 @@ function update(data) {
     }
 }
 
-function connectWebSocket() {
-    console.log("Connecting to Websocket");
-    var websocket = new WebSocket("ws://" + location.host + "/websocket");
-    console.log("Connected to Websocket");
-
-    websocket.onopen = function(event) {
-        console.log("Trying to connect to Server");
-        websocket.send(JSON.stringify({"cmd": "loginPlayer", "loggedInPlayer": sessionStorage.getItem(str_thisPlayer)}))
-        websocket.send(JSON.stringify({"cmd": "getStatus"}))
-    }
-
-    websocket.onclose = function () {
-        console.log('Connection Closed!');
-    };
-
-    websocket.onerror = function (error) {
-        console.log('Error Occured: ' + error);
-    };
-
-    websocket.onmessage = function (e) {
-        if (typeof e.data === "string") {
-            let js = JSON.parse(e.data)
-            update(js)
-        }
-        else if (e.data instanceof ArrayBuffer) {
-            console.log('ArrayBuffer received: ' + e.data);
-            alert('ArrayBuffer received: ' + e.data)
-        }
-        else if (e.data instanceof Blob) {
-            console.log('Blob received: ' + e.data);
-            alert('Blob received: ' + e.data)
-        }
-    };
-    return websocket
-}
-
 if(sessionStorage.getItem(str_thisPlayer) == null) {
     document.location.replace("/")
 }
